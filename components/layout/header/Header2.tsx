e997de894183dc9aee823af61b358038f2fde88a
burair-ahmed <burair.s3380@gmail.com>
2024-10-31 12:56:15 +0500

    "Updated footer and header components to replace 'James.dev' with 'Burair Ahmed' and commented out 'Blog' links"

--- components/layout/header/Header2.tsx
+++ components/layout/header/Header2.tsx
@@ -16,7 +16,7 @@ export default function Header2({ scroll, isMobileMenu, handleMobileMenu,isOffCa
 							<div className="container py-3 px-4">
 								<Link className="navbar-brand d-flex main-logo align-items-center" href="/index-2">
 									<img src="assets/imgs/home-page-2/template/favicon.svg" alt="zelio" />
-									<span className="fs-4 ms-2">James.dev</span>
+									<span className="fs-4 ms-2">Burair Ahmed</span>
 								</Link>
 								<div className="d-none d-lg-flex">
 									<div className="collapse navbar-collapse" id="navbarSupportedContent">
@@ -33,9 +33,9 @@ export default function Header2({ scroll, isMobileMenu, handleMobileMenu,isOffCa
 											<li className="nav-item">
 												<Link className="nav-link" href="#portfolio">Portfolio</Link>
 											</li>
-											<li className="nav-item">
+											{/* <li className="nav-item">
 												<Link className="nav-link" href="#blog">Blog</Link>
-											</li>
+											</li> */}
 											<li className="nav-item">
 												<Link className="nav-link" href="#contact">Contact</Link>
 											</li>
